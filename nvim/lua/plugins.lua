74958d1cb4b0083d8a741ab4a8d15abade72f285
Federico Andrucci <federico.andrucci@gmail.com>
2022-10-26 11:44:32 +0200

    added neotree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -58,4 +58,13 @@ packer.startup(function(use)
     'TimUntersberger/neogit',
     requires = 'nvim-lua/plenary.nvim'
   }
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 end)
